0204345dd20cc0a91129c5f1ae907dfa286abc5c
s tech <email@sarcas.tech>
2019-09-22 20:52:17 -0400

    remove underscore prefix from constant

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 'use strict'
 
-const _alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
 
 const findIndexes = function (str) {
   let _chars = str.toLowerCase().replace(/ /g, '').split('')
   return _chars.map(_char => {
-    return _alphabet.indexOf(_char)
+    return alphabet.indexOf(_char)
   })
 }
 
@@ -16,10 +16,10 @@ module.exports = {
 
     return _charIndexes.map((val, index) => {
       let _num = val + _keyIndexes[index]
-      if (_num >= _alphabet.length) {
-        _num = _num % _alphabet.length
+      if (_num >= alphabet.length) {
+        _num = _num % alphabet.length
       }
-      return _alphabet[_num]
+      return alphabet[_num]
     }).join('')
   },
   decode: function (str, key) {
@@ -29,9 +29,9 @@ module.exports = {
     return _charIndexes.map((val, index) => {
       let _num = val - _keyIndexes[index]
       if (_num < 0) {
-        _num = _alphabet.length + _num
+        _num = alphabet.length + _num
       }
-      return _alphabet[_num]
+      return alphabet[_num]
     }).join('')
   }
 }
